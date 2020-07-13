508531f29411222b4fd3c334aa9dc31d426ab555
nadya <nadya.wyn@ya.ru>
2020-07-13 09:55:48 +0500

    Added Contacts List minification

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,21 @@ window.addEventListener('DOMContentLoaded', function () {
 		overlay.classList.add('hidden');
 	});
 
+	//Minify Contacts List 
+
+	let contactsClose = document.querySelector('.contacts__switch'),
+		contactsList = document.querySelector('.contacts__list');
+
+	contactsClose.addEventListener('click', function () {
+		if (contactsList.classList.contains('min')) {
+			contactsList.classList.remove('min');
+			contactsClose.classList.add('close');
+		} else {
+			contactsList.classList.add('min');
+			contactsClose.classList.remove('close');
+		}
+
+	});
 
 
 });
