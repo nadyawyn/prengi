179bafc76a98615ea2ccbf2653d9a19d2d59245f
nadya <nadya.wyn@ya.ru>
2020-07-15 09:13:05 +0500

    Added hiding Nav menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,25 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-	let buttonOne = document.querySelector('.button_contact1'),
-		buttonTwo = document.querySelector('.button_contact2'),
-		popup = document.querySelector('.contact-popup'),
-		popupClose = document.querySelector('.contact-popup__close'),
-		overlay = document.querySelector('.overlay');
+	/* 	let buttonOne = document.querySelector('.button_contact1'),
+			buttonTwo = document.querySelector('.button_contact2'),
+			popup = document.querySelector('.contact-popup'),
+			popupClose = document.querySelector('.contact-popup__close'),
+			overlay = document.querySelector('.overlay');
 
-	buttonOne.addEventListener('click', function () {
-		popup.classList.remove('hidden');
-		overlay.classList.remove('hidden');
-	});
-	buttonTwo.addEventListener('click', function () {
-		popup.classList.remove('hidden');
-		overlay.classList.remove('hidden');
-	});
+		buttonOne.addEventListener('click', function () {
+			popup.classList.remove('hidden');
+			overlay.classList.remove('hidden');
+		});
+		buttonTwo.addEventListener('click', function () {
+			popup.classList.remove('hidden');
+			overlay.classList.remove('hidden');
+		});
 
-	popupClose.addEventListener('click', function () {
-		popup.classList.add('hidden');
-		overlay.classList.add('hidden');
-	});
-
+		popupClose.addEventListener('click', function () {
+			popup.classList.add('hidden');
+			overlay.classList.add('hidden');
+		});
+	 */
 	//Minify Contacts List 
 
 	let contactsClose = document.querySelector('.contacts__switch'),
@@ -35,6 +35,42 @@ window.addEventListener('DOMContentLoaded', function () {
 		}
 
 	});
+
+	//Nav Menu
+
+	let navClose = document.querySelector('.header__nav-close'),
+		navMenu = document.querySelector('.header__nav-menu');
+
+	if (window.matchMedia('(max-width: 768px)').matches) {
+		navMenu.classList.add('hidden');
+	}
+
+	window.addEventListener(`resize`, event => {
+		if (window.matchMedia('(max-width: 768px)').matches) {
+			navMenu.classList.add('hidden');
+		} else {
+			navMenu.classList.remove('hidden');
+		}
+	}, false);
+
+	navClose.addEventListener('click', function () {
+		if (navMenu.classList.contains('hidden')) {
+			navMenu.classList.remove('hidden');
+			navClose.classList.add('close');
+		} else {
+			navMenu.classList.add('hidden');
+			navClose.classList.remove('close');
+		}
+	});
+	/* navClose.addEventListener('click', function () {
+		if (navMenu.style.display == 'none') {
+			alert('None');
+		} else {
+			let chc = navMenu.style.display;
+			alert(chc);
+		}
+	}); */
+
 
 
 });
